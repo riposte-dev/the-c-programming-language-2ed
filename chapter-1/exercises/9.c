cdf56d035b64b62920e89749371fbb63e5c2afb8
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-21 15:23:37 +0900

    Replace one or more blanks by a single blank

---
+++ chapter-1/exercises/9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, pc;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' && pc == ' ') {
+            ;
+        } else {
+            putchar(c);
+        }
+        pc = c;
+    }
+}
