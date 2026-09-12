fd36bcb66e58cc78b47dd3e41f4d0f87dab5e58b
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-09-12 10:41:40 +0900

    Initialize an array of word lengths

--- chapter-1/exercises/13.c
+++ chapter-1/exercises/13.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 10 /* maximum possible length of a word */
 #define IN 1 /* inside a word */
 #define OUT 0 /* outside a word */
 
 int main() {
     int c, state;
+    int lengths[MAX_LENGTH];
+
+    // A word needs to be at least one letter
+    for (int i = 1; i < MAX_LENGTH + 1; ++i) {
+        lengths[i] = 0;
+        printf("(%d, %d) ", i, lengths[i]);
+    }
 
     state = OUT;
     while ((c = getchar()) != EOF) {
