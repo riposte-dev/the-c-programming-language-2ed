9d364261762d2b18e950271b08a79d026251dd64
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-09-12 12:41:22 +0900

    Remove unused variable state

--- chapter-1/exercises/13.c
+++ chapter-1/exercises/13.c
@@ -7,28 +7,21 @@ a vertical orientation is more challenging.
 #include <stdio.h>
 
 #define MAX_LENGTH 20 // Maximum length of a word
-#define IN 1 // Inside a word
-#define OUT 0 // Outside a word
 
 int main() {
-    int c, state, length;
+    int c, length;
     int lengths[MAX_LENGTH];
 
     // Initialize array (A word needs to be at least one letter, so i > 0)
     for (int i = 1; i < MAX_LENGTH + 1; ++i)
         lengths[i] = 0;
 
-    state = OUT;
     length = 0;
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t') {
-            state = OUT;
-
             ++lengths[length];
             length = 0; // Reset length counter when c is outside a word
         } else {
-            state = IN;
-
             ++length; // So long as c is inside a word, continue incrementing the length
         }
     }
