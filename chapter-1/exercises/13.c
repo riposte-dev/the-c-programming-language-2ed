a4752ca9599d1a18a20e4b17323afbaad9e03b97
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-09-12 10:43:07 +0900

    Update comments

--- chapter-1/exercises/13.c
+++ chapter-1/exercises/13.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-#define MAX_LENGTH 10 /* maximum possible length of a word */
-#define IN 1 /* inside a word */
-#define OUT 0 /* outside a word */
+#define MAX_LENGTH 10 // Maximum length of a word
+#define IN 1 // Inside a word
+#define OUT 0 // Outside a word
 
 int main() {
     int c, state;
     int lengths[MAX_LENGTH];
 
-    // A word needs to be at least one letter
-    for (int i = 1; i < MAX_LENGTH + 1; ++i) {
+    // Initialize array (A word needs to be at least one letter, so i > 0)
+    for (int i = 1; i < MAX_LENGTH + 1; ++i)
         lengths[i] = 0;
-        printf("(%d, %d) ", i, lengths[i]);
-    }
 
     state = OUT;
     while ((c = getchar()) != EOF) {
