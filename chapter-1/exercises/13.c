f6dcb5eb377666a551d67de51f1476b9c1d78a76
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-09-12 10:37:29 +0900

    Check for words
    This is practically the same as 12.c

---
+++ chapter-1/exercises/13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+    int c, state;
+
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else
+            state = IN;
+
+        if (state == OUT)
+            putchar('\n');
+        else
+            putchar(c);
+    }
+
+    return 0;
+}
